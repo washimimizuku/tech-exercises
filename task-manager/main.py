918a878bcba777d73c231f3c124aeb6c9bce19e9
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-07-16 09:21:39 +0100

    Add basic task manager

--- task-manager/main.py
+++ task-manager/main.py
@@ -1,29 +1,104 @@
 #!/usr/bin/env python
 
 import logging
+import multiprocessing
 import os
 import sys
+import time
 
-from task_daemons import sleepy_daemon
+
+logfile = os.path.join(os.getcwd(), "task_manager.log")
+logging.basicConfig(filename=logfile, level=logging.DEBUG)
+
+
+def daemon():
+
+    while True:
+        time.sleep(1)
+        logging.info(f'running process with pid {os.getpid()} ...')
+
+
+class TaskManager:
+    def __init__(self, limit):
+        self.processes = []
+        self.limit = limit
+
+    def add_process(self, level):
+        if (len(self.processes) < self.limit):
+            process = Process(level)
+            self.processes.append(process)
+            print(f'Added process {process.pid}')
+        else:
+            print(f'Limit of {self.limit} processes reached')
+
+    def list_processes(self):
+        for process in self.processes:
+            print(f'{process.pid} | {process.priority} | {process.proc.is_alive()}')
+
+    def kill_process(self, pid):
+        for index, process in enumerate(self.processes):
+            if process.pid == pid:
+                process.stop()
+                del self.processes[index]
+                return
+
+    def kill_processes(self, priority):
+        to_delete = []
+        for index, process in enumerate(self.processes):
+            if process.priority == priority:
+                to_delete.append(index)
+                process.stop()
+
+        for index in reversed(to_delete):
+            del self.processes[index]
+
+    def kill_all_processes(self):
+        for process in self.processes:
+            process.stop()
+        self.processes = []
+
+
+class Process:
+    def __init__(self, priority):
+        self.pid = 0
+        self.priority = priority
+        self.proc = multiprocessing.Process(target=daemon, args=())
+        self.proc.daemon = True
+        self.proc.start()
+        self.pid = self.proc.pid
+
+    def stop(self):
+        self.proc.kill()
 
 
 if __name__ == '__main__':
 
-    action = sys.argv[1]
-    logfile = os.path.join(os.getcwd(), "sleepy.log")
-    pidfile = os.path.join(os.getcwd(), "sleepy.pid")
+    limit = int(sys.argv[1])
 
-    logging.basicConfig(filename=logfile, level=logging.DEBUG)
-    d = sleepy_daemon.SleepyDaemon(pidfile=pidfile)
+    while True:
+        action = input('> ')
 
-    if action == "start":
+        if action[:4] == "add ":
+            level = action.split()[1]
+            task_manager.add_process(level)
 
-        d.start()
+        elif action == "list":
+            task_manager.list_processes()
 
-    elif action == "stop":
+        elif action[:5] == "kill ":
+            pid = int(action.split()[1])
+            task_manager.kill_process(pid)
 
-        d.stop()
+        elif action[:10] == "killgroup ":
+            priority = action.split()[1]
+            task_manager.kill_processes(priority)
 
-    elif action == "restart":
+        elif action == "killall":
+            task_manager.kill_all_processes()
 
-        d.restart()
+        elif action == "quit":
+            task_manager.kill_all_processes()
+            break
+
+        else:
+            print("Command not found, try again")
